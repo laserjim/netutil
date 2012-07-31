c2342547ef116a62eddafeda2f28041fe48446ba
Jim <jim@XR9-2>
2012-07-30 21:27:42 -0700

    Added a NetworkInterfaceManager responsible for managing operating system dependent functionality such as bring up/down nics and setting addresses.

---
+++ src/info/netutil/NetworkInterfaceManager.java
@@ -0,0 +1,35 @@
+package info.netutil;
+
+import java.io.IOException;
+import java.net.Inet4Address;
+import java.net.NetworkInterface;
+
+public class NetworkInterfaceManager
+{
+	public static void setIpAddress(NetworkInterface nic, Inet4Address ip) throws InterruptedException, IOException
+	{
+		String ipString = ip.getAddress()[0]+"."+ip.getAddress()[1]+"."+ip.getAddress()[2]+"."+ip.getAddress()[3];
+		
+		// TODO: Read and report errors
+		Runtime.getRuntime().exec(new String[]{"ifconfig", nic.getName(), "inet", ipString}).waitFor();
+	}
+	
+	public static void dhclient(NetworkInterface nic) throws InterruptedException, IOException
+	{
+		// TODO: Read and report errors
+		Runtime.getRuntime().exec(new String[]{"dhclient", nic.getName()}).waitFor();
+	}
+	
+	public static void takeDown(NetworkInterface nic) throws InterruptedException, IOException
+	{
+		// TODO: Read and report errors
+		Runtime.getRuntime().exec(new String[]{"ifconfig", nic.getName(), "down"}).waitFor();
+	}
+	
+	public static void bringUp(NetworkInterface nic) throws InterruptedException, IOException
+	{
+		// TODO: Read and report errors
+		Runtime.getRuntime().exec(new String[]{"ifconfig", nic.getName(), "up"}).waitFor();
+	}
+}
+
